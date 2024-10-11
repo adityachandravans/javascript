fba3cb49604a0f00dcf373dd71e59f9e46df5892
adityachandravans <chandravanshiaditya25@gmail.com>
2024-10-11 12:37:03 +0530

    changes saved

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ for(i=0;i<myNodelist.length;i++){
     myNodelist[i].appendChild(span);
 }
 
+var close=document.getElementsByClassName("close");
+var i;
+for(i=0;i<close.length;i++){
+close[i].onclick=function(){
+    var div=this.parentElement;
+    div.style.display="none";
+};
+}
